fc55d388a1161a144fbeaf1b6da879a46a153412
Jacob Bergeron <jebergeron3775@gmail.com>
2026-03-31 15:06:31 -0500

    Update MongoDB connection details and database namedfs

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://1234:password1234@cluster0.yizjk8q.mongodb.net/?appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -33,8 +33,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('1234');
+    const parts = database.collection('1234');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
